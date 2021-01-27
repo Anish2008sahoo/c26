c346dfa4d45acda0f8c63b809d4e4180f35f1e9a
Anish Sahoo <anishsahoo2008@gmail.com>
2021-01-27 18:37:34 +0530

    sucess

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ function setup(){
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+    log6 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -48,7 +48,7 @@ function draw(){
     box2.display();
     ground.display();
     pig1.display();
-    log1.display();
+    log6.display();
 
     box3.display();
     box4.display();
